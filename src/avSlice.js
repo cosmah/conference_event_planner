54732f89403ed81a7556ebfba980ccba64544ed0
cosmah <cosmahke4@gmail.com>
2024-08-19 15:40:40 +0300

    first commit

---
+++ src/avSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const avSlice = createSlice({
+  name: "av",
+  initialState: [
+    
+  ],
+
+
+  reducers: {
+    incrementAvQuantity: (state, action) => {
+      const item = state[action.payload];
+      if(item){
+        item.quantity++;
+      }
+    },
+    decrementAvQuantity: (state, action) => {
+      const item = state[action.payload];
+      if(item && item.quantity > 0){
+        item.quantity--;
+      }
+    },
+  },
+});
+
+export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+
+export default avSlice.reducer;
